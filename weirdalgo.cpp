22c850bf1628a122f47a758c6f2808039d962006
Ritvik Bharti <bhartiritvik1000@gmail.com>
2025-01-28 12:28:33 +0530

    first commit

---
+++ weirdalgo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    cout << n << " ";
+    while(n > 1){
+        if(n %2 == 0){
+            cout << n/2 <<" ";
+            n/=2;
+        }else{
+            cout << n*3 + 1 <<" ";
+            n = n*3 + 1;
+        }
+    }
+}
